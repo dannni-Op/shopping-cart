817aa7c321e9b936075ccf778e92ac528d2656cd
lord <dannncode@gmail.com>
2024-08-10 12:04:23 +0700

    feat: add  search icon

--- src/components/searchbar.jsx
+++ src/components/searchbar.jsx
@@ -4,14 +4,14 @@ export function SearchBar() {
   return (
     <>
       <section className="w-full">
-        <form className="w-full">
+        <form className="w-full relative">
           <input
             type="search"
-            className="p-4 w-full rounded-xl"
+            className="p-4 w-full rounded-xl shadow ps-14"
             placeholder="Enter product name..."
           />
+          <FaSearch className="text-lg text-slate-300 absolute top-1/3 left-6" />
         </form>
-        {/* <FaSearch className="text-2xl text-slate-300" /> */}
       </section>
     </>
   );
