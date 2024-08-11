e5a8ca883ed6978c238c2189180f2c518bb7b9ce
lord <dannncode@gmail.com>
2024-08-11 10:52:22 +0700

    feat: create settings ui

--- src/pages/settings.jsx
+++ src/pages/settings.jsx
@@ -1,10 +1,15 @@
 import { Navbar } from "../components/navbar";
+import { FaCode } from "react-icons/fa";
 
 export function Settings() {
   return (
     <>
       <Navbar />
-      <h1>Coming Soon.</h1>
+      <section className="pg-slate-50 w-full h-screen flex flex-col justify-center items-center gap-4">
+        <FaCode className="text-9xl text-blue-500" />
+        <h2 className="text-blue-500 font-extrabold text-4xl">We Are Coding</h2>
+        <p className="text-slate-400 font-medium">Coming Soon!</p>
+      </section>
     </>
   );
 }
